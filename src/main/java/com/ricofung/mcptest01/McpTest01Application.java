51ad45c596ff4441f32ee9de759b6a6df93fac39
RicoFung <ricofungcn@icloud.com>
2025-04-18 10:40:43 +0800

    Add Spring Boot main application class

---
+++ src/main/java/com/ricofung/mcptest01/McpTest01Application.java
@@ -0,0 +1,13 @@
+package com.ricofung.mcptest01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McpTest01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McpTest01Application.class, args);
+    }
+
+}
